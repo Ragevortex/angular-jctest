b05d855b1499014cf6eab1ed2448c1749325e54b
Ragevortex <peniaman@gmail.com>
2019-11-06 23:09:49 -0500

    3 files modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -3,19 +3,24 @@ export const products = [
     image:'https://www.bing.com/th?id=OP.5kvHvRWVYz8olA474C474&w=180&h=150&rs=1&o=5&pid=21.1' ,
     name: 'Honey Wheat Bread',
     price: 3.99,
-    description: 'A bag of sliced Hone wheat Bread'
+    description: 'A bag of sliced Hone wheat Bread',
+    onhand:0
   },
-  {
-    image:'https://scene7.targetimg1.com/is/image/Target/12920997?wid=480&hei=480',
+   {
+    image:'https://www.bing.com/th?id=OP.8HUCdv8SznDSsg474C474&w=180&h=150&rs=1&o=5&pid=21.1',
     name: 'Cereal',
     price: 3.99,
-    description: 'A Box of some cereal of our prefference'
+    description: 'Cereal crunchy granola and raisins.',
+    onhand:0
+
   },
   {
     image:'https://www.bing.com/th?id=OIP.jRPmQSfkhmIoH5S5s20OYwHaIH&w=186&h=203&c=7&o=5&pid=1.7',
     name: 'Pancakes',
     price: 2.99,
-    description: 'A bag of pre-made pancakes.'
+    description: 'A bag of pre-made pancakes.',
+    onhand:0
+
   }
 ];
 
